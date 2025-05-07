6ac61f44da83f8895ecf0c06f58c7fb98cb7b65b
BasireddyKarthik <karthikdeveloper45@gmail.com>
2025-05-07 08:48:24 +0530

    initi

--- app/src/main/java/pzn/project/ap/myapplication888888/MainActivity.kt
+++ app/src/main/java/pzn/project/ap/myapplication888888/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplication888888Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Andrskudfhuassdhfuygoid",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
